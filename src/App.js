2da795e56300b18d8e64388600951bb4e3d4a470
haojun14 <haojun0114@outlook.com>
2024-02-19 11:24:52 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import React from 'react';
 import './App.css';
-
-function App() {
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+const App = () =>{
   return (
-    <div className="App"></div>
+    <>
+    <Header />
+    <main className='main'>
+      <Home />
+    </main>
+    </>
   );
 }
 
