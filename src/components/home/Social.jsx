8462b238cfa5c349b0c6446b89bae766304f78fa
haojun14 <haojun0114@outlook.com>
2024-02-22 10:34:17 +0800

    homepage-done

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Social = () => {
-    return (
-        <div className="home__social">
-            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
-            <i class="uil uil-instagram"></i>
-            </a>
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
 
-            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
-            <i class="uil uil-dribbble"></i>
-            </a>
+      <a
+        href="https://www.instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
 
-            <a href="https://www.github.com/" className="home__social-icon" target="_blank" rel="noopener noreferrer">
-            <i class="uil uil-github-alt"></i>
-            </a>
-        </div>
-    );
+      <a
+        href="https://www.github.com/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
 };
 
 export default Social;
